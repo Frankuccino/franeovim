6c01626ace98fe6bb1cb2b5fd72652a9aa4493a5
Frank Angelo Malubag <frankabad189@gmail.com>
2026-05-25 17:27:28 +0800

    feat: add emmet-ls after installing via Mason to enable usage.
    This allows shorthand-to-code features through an Emmet way instead of
    manual typing.
    
    This is enabled for filetypes of html, css, js, ts, js-react, ts-react.

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -1,9 +1,13 @@
+-- This is the file where you add and enable LSPs from Mason.
 -- 1. Initialize the configurations for your engines
 vim.lsp.config('html', {})
 vim.lsp.config('cssls', {})
 vim.lsp.config('vtsls', {})
-
+vim.lsp.config('emmet_ls', {
+  filetypes = { 'html', 'css', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' },
+})
 -- 2. Physically enable them to run on their respective file types
 vim.lsp.enable 'html'
 vim.lsp.enable 'cssls'
 vim.lsp.enable 'vtsls'
+vim.lsp.enable 'emmet_ls'
