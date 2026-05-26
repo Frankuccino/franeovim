a1c220aef20fdb97ff2d6c27efbf9b3fc1d1dce6
Frank Angelo Malubag <frankabad189@gmail.com>
2026-05-26 19:30:55 +0800

    feat: add ui color set for CursorLine and Telescope in theme

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -14,3 +14,8 @@ vim.g.gruvbox_material_better_performance = 1
 
 -- 3. Set the colorscheme live
 vim.cmd 'colorscheme gruvbox-material'
+vim.opt.cursorline = true
+vim.api.nvim_set_hl(0, 'WinSeparator', { fg = 'none', bg = 'none' })
+vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#2d252b' })
+vim.api.nvim_set_hl(0, 'TelescopeSelection', { bg = '#2d252b', fg = '#fbf1c7' })
+vim.api.nvim_set_hl(0, 'TelescopeSelectionCaret', { fg = '#eb5ba3', bg = '#2d252b' })
