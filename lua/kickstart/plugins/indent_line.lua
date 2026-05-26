f8be9af9eb5e1c8efcf00ddecb0734223eac7377
Frank Angelo Malubag <frankabad189@gmail.com>
2026-05-26 15:38:32 +0800

    feat: improve indentline UI to show necessary indentline only.

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -3,4 +3,42 @@
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help ibl`
 vim.pack.add { 'https://github.com/lukas-reineke/indent-blankline.nvim' }
-require('ibl').setup {}
+local hooks = require 'ibl.hooks'
+
+-- Only color for scope (when cursor is in block)
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  vim.api.nvim_set_hl(0, 'IblScope', { fg = '#665c54' }) -- Subtle gruvbox gray
+end)
+
+require('ibl').setup {
+  enabled = true,
+
+  -- Disable all regular indent guides
+  indent = {
+    char = ' ', -- Space = invisible
+  },
+
+  -- Only show scope (current block with cursor)
+  scope = {
+    enabled = true,
+    char = '│',
+    show_start = false,
+    show_end = false,
+    highlight = 'IblScope',
+  },
+
+  exclude = {
+    filetypes = {
+      'help',
+      'neo-tree',
+      'TelescopePrompt',
+      'TelescopeResults',
+      'dashboard',
+      '',
+    },
+    buftypes = {
+      'terminal',
+      'nofile',
+    },
+  },
+}
