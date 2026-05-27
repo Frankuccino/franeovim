5a4617f67098d3d52dddea18d1e4ebc5da143d7a
Frank Angelo Malubag <frankabad189@gmail.com>
2026-05-27 23:23:50 +0800

    feat: add formatter for json, javascript, typescript, and (react)ts/js

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -2,11 +2,20 @@ local conform = require 'conform'
 
 conform.setup {
   formatters_by_ft = {
+    lua = { 'stlua' },
     html = { 'prettier' },
     css = { 'prettier' },
-    javascript = { 'prettier' },
     rust = { 'rustfmt' },
+    json = { 'prettier' },
+    jsonc = { 'prettier' },
+
+    -- ⚛️ React & JavaScript Filetypes
+    javascript = { 'prettier' },
+    typescript = { 'prettier' },
+    javascriptreact = { 'prettier' }, -- ✨ Handles .jsx files
+    typescriptreact = { 'prettier' }, -- ✨ Handles .tsx files
   },
+
   -- Automatically format when you write the file (:w)
   format_on_save = {
     lsp_fallback = true,
